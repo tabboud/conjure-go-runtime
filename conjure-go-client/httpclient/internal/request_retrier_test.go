0a33f77d87358d7461079b268ab0a7bedca6eb42
Brad Moylan <bmoylan@palantir.com>
2021-06-16 08:42:28 -0700

    wiggle room

--- conjure-go-client/httpclient/internal/request_retrier_test.go
+++ conjure-go-client/httpclient/internal/request_retrier_test.go
@@ -57,24 +57,24 @@ func TestRequestRetrier_UnlimitedAttempts(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	r := NewRequestRetrier([]string{"https://example.com"}, retry.Start(ctx, retry.WithInitialBackoff(100*time.Millisecond)), 0)
+	r := NewRequestRetrier([]string{"https://example.com"}, retry.Start(ctx, retry.WithInitialBackoff(200*time.Millisecond)), 0)
 
 	startTime := time.Now()
 	uri, _ := r.GetNextURI(nil, nil)
 	require.Equal(t, uri, "https://example.com")
-	require.Lessf(t, time.Since(startTime), 99*time.Millisecond, "first GetNextURI should not have any delay")
+	require.Lessf(t, time.Since(startTime), 199*time.Millisecond, "first GetNextURI should not have any delay")
 
 	startTime = time.Now()
 	uri, _ = r.GetNextURI(nil, nil)
 	require.Equal(t, uri, "https://example.com")
-	assert.Greater(t, time.Since(startTime), 100*time.Millisecond, "delay should be at least 1 backoff")
-	assert.Less(t, time.Since(startTime), 200*time.Millisecond, "delay should be less than 2 backoffs")
+	assert.Greater(t, time.Since(startTime), 200*time.Millisecond, "delay should be at least 1 backoff")
+	assert.Less(t, time.Since(startTime), 400*time.Millisecond, "delay should be less than 2 backoffs")
 
 	startTime = time.Now()
 	uri, _ = r.GetNextURI(nil, nil)
 	require.Equal(t, uri, "https://example.com")
-	assert.Greater(t, time.Since(startTime), 200*time.Millisecond, "delay should be at least 2 backoffs")
-	assert.Less(t, time.Since(startTime), 400*time.Millisecond, "delay should be less than 3 backoffs")
+	assert.Greater(t, time.Since(startTime), 400*time.Millisecond, "delay should be at least 2 backoffs")
+	assert.Less(t, time.Since(startTime), 800*time.Millisecond, "delay should be less than 3 backoffs")
 
 	// Success should stop retries
 	uri, _ = r.GetNextURI(&http.Response{StatusCode: 200}, nil)
